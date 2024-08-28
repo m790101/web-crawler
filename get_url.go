7a144ebc58edede90f376629272544499fa92d73
鄭皓文 HaoWen Cheng <haowen.cheng@tpisoftware.com>
2024-08-28 11:48:49 +0800

    update getUrl function with test

--- get_url.go
+++ get_url.go
@@ -1,43 +1,57 @@
 package main
 
 import (
+	"fmt"
+	"net/url"
 	"strings"
 
 	"golang.org/x/net/html"
 )
 
 func getURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error) {
+
+	baseURL, err := url.Parse(rawBaseURL)
+	if err != nil {
+
+		return nil, fmt.Errorf("couldn't parse base URL")
+	}
+
 	doc, err := html.Parse(strings.NewReader(htmlBody))
 
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("couldn't parse HTML")
 	}
 
-	urls := getURLsFromNode(doc, []string{}, &rawBaseURL)
+	urls, _ := getURLsFromNode(doc, []string{}, baseURL)
 
 	return urls, nil
 }
 
-func getURLsFromNode(node *html.Node, urls []string, rawBaseURL *string) []string {
+func getURLsFromNode(node *html.Node, urls []string, baseURL *url.URL) ([]string, error) {
 
 	if node.Type == html.ElementNode && node.Data == "a" {
 		for _, a := range node.Attr {
 			if a.Key == "href" {
-				rawBaseURL := *rawBaseURL
-				if !strings.HasPrefix(a.Val, "https://") {
-					a.Val = rawBaseURL + a.Val
+				href, err := url.Parse(a.Val)
+				if err != nil {
+					fmt.Printf("couldn't parse href '%v': %v\n", a.Val, err)
+					continue
 				}
-				urls = append(urls, a.Val)
+				resolvedURL := baseURL.ResolveReference(href)
+				urls = append(urls, resolvedURL.String())
+
 			}
 		}
 	}
 
 	if node.FirstChild != nil {
-		urls = getURLsFromNode(node.FirstChild, urls, rawBaseURL)
+		urls, _ = getURLsFromNode(node.FirstChild, urls, baseURL)
 	}
 	if node.NextSibling != nil {
-		urls = getURLsFromNode(node.NextSibling, urls, rawBaseURL)
+		urls, _ = getURLsFromNode(node.NextSibling, urls, baseURL)
 	}
-
-	return urls
+	if len(urls) == 0 {
+		return nil, nil
+	}
+	return urls, nil
 }
